05d534f4484663b2cb76f5ea35b916d3c0c1832b
Keerthana <98882146+KeerthuMG@users.noreply.github.com>
2022-02-25 18:03:43 +0530

    Add files via upload

---
+++ MiniProject_C/3_Implementation/src/calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void addition ();
+void subtraction ();
+void multiplication ();
+void division ();
+void square ();
+void cube();
+void modulus ();
+void factorial ();
+void sel_func (int);
