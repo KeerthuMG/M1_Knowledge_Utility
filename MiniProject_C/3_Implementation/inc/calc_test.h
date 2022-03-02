4554c7ec28e1a93f0b2dae20a0db808c415ff07d
Keerthana <98882146+KeerthuMG@users.noreply.github.com>
2022-03-02 19:58:51 +0530

    Add files via upload

---
+++ MiniProject_C/3_Implementation/inc/calc_test.h
@@ -0,0 +1,16 @@
+#ifndef __CALC_TEST_H__
+#define __CALC_TEST_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int addition(int n1, int n2);
+
+int subtraction(int n1, int n2);
+
+int multiplication(int n1, int n2);
+
+int division(int n1, int n2);
+
+#endif 
