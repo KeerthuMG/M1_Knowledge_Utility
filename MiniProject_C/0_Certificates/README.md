4be89f6af30a558e3ed0ae0302e5328c9505abea
Keerthana <98882146+KeerthuMG@users.noreply.github.com>
2022-03-04 12:57:34 +0530

    Delete MiniProject_C directory

--- MiniProject_C/0_Certificates/README.md
+++
@@ -1,6 +0,0 @@
-# Certificates:
-
-1. Sololearn
-2. Hackerearth
-3. NDG Linux Unhatched
-4. GitHub Learning
--- MiniProject_C/3_Implementation/inc/calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULATOR_H_
-#define CALCULATOR_H_
-
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#endif
